11ae35cdf028cfd77fd57dd14a336068016ea4d9
osama.elmorady <osama.m.elmorady@gmail.com>
2024-10-22 20:18:17 +0200

    [0001] Update CMAKE files, Switch SWC into C++

--- 02-Source/01-Dep/Config.cmake
+++ 02-Source/01-Dep/Config.cmake
@@ -21,8 +21,8 @@
 
 
 #################### Set Language #################### 
-set(PROJECT_LANGUAGE "C")
-set(PROJECT_LANGUAGE_VERSION 90)
+set(PROJECT_LANGUAGE "CXX")
+set(PROJECT_LANGUAGE_VERSION "11")
 
 
 ################ Set project type, Name ############### 
@@ -43,14 +43,34 @@ set(COMPILER_TYPE "WINDOWS")
 set(COMPILER_VERSION "14.1.0")
 set(COMPILER_PATH "C:/msys64/ucrt64")
 
+# set(CMAKE_DEBUG_TOOL_PATH "C:/WinAVR-20100110/bin")
+# set(CMAKE_DEBUG_TOOL_TYPE "AVRDUDE")
+# set(CMAKE_FLASH_TOOL_PATH "C:/WinAVR-20100110/bin")
+# set(CMAKE_FLASH_TOOL_TYPE "AVRDUDE")
 
 
 
-# # #####  Set Microcontroller options ##### 
-# set(ARCHITECTURE "WINDOWS")
-# set(MICROCONTROLLER "WINDOWS")
-# set(ROM_SIZE 1MB)
-# set(RAM_SIZE 65K)
+###############  Set Compiler Flags ############### 
+# set(CUSTOM_C_FLAGS  -mmcu=$(MCU) -DF_CPU=$(F_CPU)UL -DBAUD=$(BAUD)  -Os )
+# set(CUSTOM_CXX_FLAGS -fno-exceptions -mmcu=$(MCU) -DF_CPU=$(F_CPU)UL -DBAUD=$(BAUD)  -Os -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--relax)
+# set(CUSTOM_ASM_FLAGS -C -G)
+# set(CUSTOM_LINKER_FLAGS -C -Olink)
+
+
+
+###############  Set Linker script file ############### 
+set(LINKER_FILE linker_script.ld)
+
+
+
+
+
+
+# #####  Set Microcontroller options ##### 
+set(ARCHITECTURE "WINDOWS")
+set(MICROCONTROLLER "WINDOWS")
+set(ROM_SIZE 1MB)
+set(RAM_SIZE 65K)
 
 
 
